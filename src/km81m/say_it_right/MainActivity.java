5475284767a5abf4eea039ca0e6075ae968905b6
Alexey Dushenin <alexey.dushenin@gmail.com>
2013-11-02 15:58:42 +0200

    empty project

---
+++ src/km81m/say_it_right/MainActivity.java
@@ -0,0 +1,15 @@
+package km81m.say_it_right;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
