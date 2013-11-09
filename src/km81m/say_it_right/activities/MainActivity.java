4a6059f5e4e3184907885151ff9f5fbeaebcce10
Alexey Dushenin <alexey.dushenin@gmail.com>
2013-11-09 18:29:24 +0200

    main page fix

--- src/km81m/say_it_right/activities/MainActivity.java
+++ src/km81m/say_it_right/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import km81m.say_it_right.R;
 
 public class MainActivity extends Activity {
@@ -14,6 +16,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.settings_level);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.level_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     public void startTest(View view) {
